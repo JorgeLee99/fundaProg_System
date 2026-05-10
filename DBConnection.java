a4ab445065c4f1ce4feb59334c939bd187cb5593
Kaizer2107 <ninogonjoran01@gmail.com>
2026-05-10 16:08:30 +0800

    simplified getConnection function

--- DBConnection.java
+++ DBConnection.java
@@ -6,18 +6,12 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL      = "jdbc:mysql://localhost:3306/barangay_db";
-    private static final String USER     = "root";
-    private static final String PASSWORD = "";
-
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("[ERROR] MySQL JDBC Driver not found.");
-            e.printStackTrace();
-        }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/barangay_db",
+            "root",
+            ""
+        );
     }
 
     public static void closeConnection(Connection conn) {
