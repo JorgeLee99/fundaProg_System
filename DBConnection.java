de58b3870f2b9d03aa9ee105c2e39d257790b7bb
Kaizer2107 <ninogonjoran01@gmail.com>
2026-05-26 08:25:10 +0800

    Restore deleted files from database-engineer branch

---
+++ DBConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/barangay_db",
+            "root",
+            ""
+        );
+    }
+
+    public static void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println("[ERROR] Failed to close connection.");
+                e.printStackTrace();
+            }
+        }
+    }
+}
